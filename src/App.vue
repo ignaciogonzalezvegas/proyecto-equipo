5ffcc94b28c34a503111eef9b52a89fb46f8e048
Ignacio Gonzalez <ijgv33@gmail.com>
2021-04-26 23:44:49 -0400

    cambio en nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Inicio</router-link> |
-      <router-link to="/equipo">Equipo</router-link> |
-      <router-link to="/circuitos">Circuitos</router-link> |
-      <router-link to="/instituciones">Instituciones</router-link> |
-      <router-link to="/poblacion">Poblacion</router-link>
-    </div>
-    <router-view/>
+    <b-navbar toggleable="lg" type="light " variant="primary">
+      <img alt="Vue logo" src="@/assets/logo.png" />
+      <b-navbar-brand href="#">EIS GRAN MARISCAL</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav id="nav" class="ml-auto">
+          <router-link to="/">Inicio</router-link> |
+          <router-link to="/equipo">Equipo</router-link> |
+          <router-link to="/circuitos">Circuitos</router-link> |
+          <router-link to="/instituciones">Instituciones</router-link> |
+          <router-link to="/poblacion">Poblacion</router-link>
+          <b-nav-item-dropdown left>
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
   </div>
 </template>
 
@@ -22,14 +36,23 @@
 
 #nav {
   padding: 30px;
+  text-align: initial;
+  background-color: ivory;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #0a0a0a;
+  background-color: lightblue;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #f1f1f5;
+}
+
+img {
+  width: 60px;
+  height: 60px;
+  margin-right: 2%;
 }
 </style>
--- src/views/Equipo.vue
+++ src/views/Equipo.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="home">
+  <div>
     <h3>Informaciòn institucional</h3>
     <!-- //tajeta de informacón institucional -->
-    <b-card class="text-center">
+    <b-card class="text-left">
       <div class="bg-secondary text-light">
         <p>
           CÓDIGO DEA: OD19251914
           CÓDIGO ADMINISTRATIVO: 006490428
           CÓDIGO ESTADÍSTICO: 191666
-
           CORREO ELECTRÓNICO: eisgma20108@gmail.com
-
           DIRECCIÓN: Av. Miranda, dentro del ETC "Modesto Silva"
         </p>
       </div>
