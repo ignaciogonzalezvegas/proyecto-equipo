7b7376522a3bbfa20fce05b9e9ca7f7e5c37b2cf
Ignacio_Gonzalez <ijgv33@gmail.com>
2021-04-28 14:17:12 -0400

    tienda 28042021

--- src/router/index.js
+++ src/router/index.js
@@ -42,14 +42,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Poblacion.vue')
   },
-  {
-    path: '/trabajador',
-    name: 'trabajador',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/DetalleTrabajador.vue')
-  }
+
 ]
 
 const router = new VueRouter({
--- src/views/Circuitos.vue
+++ src/views/Circuitos.vue
@@ -6,16 +6,41 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-      }
-    }
-  }
+export default {
+  data() {
+    return {
+      items: [
+        { Circuito: 40, Nombre: "Dickerson" },
+        { Circuito: 21, Nombre: "Larsen" },
+        { Circuito: 89, Nombre: "Geneva" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+        { Circuito: 38, Nombre: "Jami" },
+      ],
+    };
+  },
+};
 </script>
+<style></style>
