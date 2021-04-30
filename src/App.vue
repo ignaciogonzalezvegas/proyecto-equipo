bab7157bcb77047660c00b540f9984c221a37c69
Ignacio Gonzalez <ijgv33@gmail.com>
2021-04-29 21:18:00 -0400

    casa 290420

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="light " variant="primary">
+    <b-navbar toggleable="lg" type="light " variant="primary" >
       <img alt="Vue logo" src="@/assets/logo.png" />
       <b-navbar-brand href="#">EIS GRAN MARISCAL</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav id="nav" class="ml-auto">
-          <router-link to="/">Inicio</router-link> |
+        <b-nav-text >
+          <router-link to="/">Inicio</router-link>    |
           <router-link to="/equipo">Equipo</router-link> |
           <router-link to="/circuitos">Circuitos</router-link> |
           <router-link to="/instituciones">Instituciones</router-link> |
           <router-link to="/poblacion">Poblacion</router-link>
+        </b-nav-text>
+        <b-navbar-nav id="nav" class="ml-auto ">
+
           <b-nav-item-dropdown left>
             <template #button-content>
               <em>User</em>
@@ -41,7 +44,7 @@
   background-color: ivory;
 }
 */
-#nav a {
+/*#nav a {
   font-weight: bold;
   color: #0a0a0a;
  
@@ -50,7 +53,7 @@
 #nav a.router-link-exact-active {
   color: #f1f1f5;
 }
-
+*/
 img {
   width: 60px;
   height: 60px;
